436780d8b5a99b544e7f08e66d9b0165606873a0
ninad-moree <ninadyogeshmore@gmail.com>
2024-04-04 17:54:44 +0530

    Minor Changes Done

--- DSBDAL/Assignment 12/WeatherDataAverage.java
+++ DSBDAL/Assignment 12/WeatherDataAverage.java
@@ -5,20 +5,6 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
-// import java.io.IOException;
-
-// import org.apache.hadoop.conf.Configuration;
-// import org.apache.hadoop.fs.Path;
-// import org.apache.hadoop.io.FloatWritable;
-// import org.apache.hadoop.io.Text;
-// import org.apache.hadoop.mapreduce.Job;
-// import org.apache.hadoop.mapreduce.Mapper;
-// import org.apache.hadoop.mapreduce.Reducer;
-// import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-// import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-
-
 public class WeatherDataAverage {
 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, FloatWritable> {
