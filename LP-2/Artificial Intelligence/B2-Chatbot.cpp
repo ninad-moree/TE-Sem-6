892251bdb0065e4c31971c7befc6ff038f9524a2
ninad-moree <ninadyogeshmore@gmail.com>
2024-05-08 19:28:32 +0530

    Chatbot added

--- LP-2/Artificial Intelligence/B2-Chatbot.cpp
+++ LP-2/Artificial Intelligence/B2-Chatbot.cpp
@@ -1,48 +1,68 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <ctime>
+#include <vector>
+
 using namespace std;
 
-class SimpleChatbot
-{
-public:
-    string getResponse(const string &user_input) {
-        if (containsKeyword(user_input, "hello"))
-            return "Hi there!";
-        else if (containsKeyword(user_input, "how are you"))
-            return "I'm doing well, thank you!";
-        else if (containsKeyword(user_input, "bye"))
-            return "Goodbye!";
-        else
-            return "I'm not sure how to respond to that.";
-    }
-
-private:
-    bool containsKeyword(const string &input, const string &keyword) {
-        size_t found = input.find(keyword);
-        return (found != string::npos);
-    }
+struct Product {
+    string name;
+    string description;
+    double price;
 };
 
-int main()
-{
-    SimpleChatbot chatbot;
-    string user_input;
+void displayProductList(const vector<Product>& products) {
+    cout << "Here are our products:\n";
+    for (size_t i = 0; i < products.size(); ++i) {
+        cout << i + 1 << ". " << products[i].name << endl;
+    }
+}
 
-    cout << "Simple Chatbot: Hello! Type 'bye' to exit." << endl;
+void getProductInfo(const vector<Product>& products, int selection) {
+    if (selection >= 1 && selection <= static_cast<int>(products.size())) {
+        const Product& product = products[selection - 1];
+        cout << "Product: " << product.name << endl;
+        cout << "Description: " << product.description << endl;
+        cout << "Price: $" << product.price << endl;
+    } else {
+        cout << "Invalid selection!\n";
+    }
+}
+
+int main() {
+    cout << "Welcome! How can I assist you today?\n";
+
+    // Sample product data
+    vector<Product> products = {
+        {"Product 1", "Description of Product 1", 19.99},
+        {"Product 2", "Description of Product 2", 29.99},
+        {"Product 3", "Description of Product 3", 39.99}
+    };
+
+    int selectedProduct = -1;
 
     while (true) {
-        cout << "You: ";
-        getline(std::cin, user_input);
+        string userInput;
+        cout << "\nCustomer: ";
+        getline(cin, userInput);
 
-        if (user_input == "bye") {
-            cout << "Simple Chatbot: Goodbye! Have a great day!" << endl;
-            break;
+        if (userInput.find("product") != string::npos) {
+            displayProductList(products);
+        } else if (userInput.find("select") != string::npos) {
+            int selection;
+            cout << "Enter the number of the product you want more information about: ";
+            cin >> selection;
+            cin.ignore();
+            getProductInfo(products, selection);
+            selectedProduct = selection - 1; // Remember the selected product
+        } else if (userInput.find("price") != string::npos) {
+            if (selectedProduct != -1) {
+                cout << "The price of the selected product is: $" << products[selectedProduct].price << endl;
+            } else {
+                cout << "Please select a product first!\n";
+            }
+        } else {
+            cout << "I'm sorry, I didn't understand that. How can I assist you?\n";
         }
-
-        string response = chatbot.getResponse(user_input);
-        cout << "Simple Chatbot: " << response << endl;
     }
 
     return 0;
